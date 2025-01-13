c784ba18cd1b6c9249db4ad7cc7a387a3ce6a281
Dynamic-Pistol <spidermalek666@gmail.com>
2025-01-13 12:38:19 +0200

    Overhaul interpreter

--- src/interperter/interperter.rs
+++ src/interperter/interperter.rs
@@ -14,11 +14,12 @@ use crate::{
 #[derive(Debug, Clone)]
 pub struct Interpreter {
     vars: HashMap<u64, (Object, bool)>,
+    defered_stmts: Vec<usize>,
 }
 
 impl Interpreter {
-    pub fn unary_eval(&mut self, unary: Unary) -> anyhow::Result<Object> {
-        let value = self.expr_eval(unary.right.0)?;
+    pub fn unary_eval(&mut self, unary: &Unary) -> anyhow::Result<Object> {
+        let value = self.expr_eval(&unary.right.0)?;
         use crate::ast::misc::UnaryOp::*;
         match unary.operator.0 {
             Neg => match value {
@@ -36,9 +37,9 @@ impl Interpreter {
         }
     }
 
-    pub fn binary_eval(&mut self, binary: Binary) -> anyhow::Result<Object> {
-        let lhs = self.expr_eval(binary.left.0)?;
-        let rhs = self.expr_eval(binary.right.0)?;
+    pub fn binary_eval(&mut self, binary: &Binary) -> anyhow::Result<Object> {
+        let lhs = self.expr_eval(&binary.left.0)?;
+        let rhs = self.expr_eval(&binary.right.0)?;
 
         use crate::ast::misc::BinaryOp::*;
         match binary.operator.0 {
@@ -57,7 +58,7 @@ impl Interpreter {
         }
     }
 
-    fn var_eval(&mut self, name: u64) -> anyhow::Result<Object> {
+    fn var_eval(&mut self, name: &u64) -> anyhow::Result<Object> {
         match self.vars.get(&name) {
             Some(var) => Ok(var.0),
             None => return Err(anyhow::anyhow!("Variable doesn't exist!")),
@@ -66,9 +67,9 @@ impl Interpreter {
 
     fn tenary_if_eval(
         &mut self,
-        condition: Expr,
-        value: Expr,
-        else_value: Expr,
+        condition: &Expr,
+        value: &Expr,
+        else_value: &Expr,
     ) -> anyhow::Result<Object> {
         return if self.expr_eval(condition)?.into() {
             self.expr_eval(value)
@@ -77,17 +78,17 @@ impl Interpreter {
         };
     }
 
-    pub fn expr_eval(&mut self, expr: Expr) -> anyhow::Result<Object> {
+    pub fn expr_eval(&mut self, expr: &Expr) -> anyhow::Result<Object> {
         match expr {
             Expr::Binary(b) => self.binary_eval(b),
             Expr::Literal(l) => Ok(l.value.0),
             Expr::Unary(u) => self.unary_eval(u),
-            Expr::Variable { name } => self.var_eval(name.0),
+            Expr::Variable { name } => self.var_eval(&name.0),
             Expr::TenaryIfStmt {
                 condition,
                 value,
                 else_value,
-            } => self.tenary_if_eval(condition.0, value.0, else_value.0),
+            } => self.tenary_if_eval(&condition.0, &value.0, &else_value.0),
             Expr::FunctionCall { .. } => todo!(),
         }
     }
@@ -96,15 +97,13 @@ impl Interpreter {
     //-Stmt Functions-------------------------------------------------
     //----------------------------------------------------------------
 
-    fn block_eval(&mut self, statments: Vec<Spanned<Statement>>) -> anyhow::Result<()> {
-        for statement in statments {
-            self.stmt_eval(statement.0)?
-        }
+    fn block_eval(&mut self, statments: &Vec<Spanned<Statement>>) -> anyhow::Result<()> {
+        self.stmt_eval(statments)?;
         Ok(())
     }
 
-    fn assign_eval(&mut self, assign: Assign) -> anyhow::Result<()> {
-        let val = self.expr_eval(assign.value.0)?; //Only is at top due to error about "Borrowing"
+    fn assign_eval(&mut self, assign: &Assign) -> anyhow::Result<()> {
+        let val = self.expr_eval(&assign.value.0)?; //Only is at top due to error about "Borrowing"
         let var = match self.vars.get_mut(&assign.name.0) {
             Some(o) => o,
             None => return Err(anyhow::anyhow!("Variable doesn't exist!")),
@@ -125,34 +124,34 @@ impl Interpreter {
 
     fn declar_eval(
         &mut self,
-        declaration_type: crate::ast::misc::DeclarationType,
-        name: u64,
-        _manual_type: Option<Spanned<crate::utils::valtype::Type>>,
-        value: Box<Spanned<Expr>>,
+        declaration_type: &crate::ast::misc::DeclarationType,
+        name: &u64,
+        _manual_type: &Option<Spanned<crate::utils::valtype::Type>>,
+        value: &Box<Spanned<Expr>>,
     ) -> anyhow::Result<()> {
-        if self.vars.contains_key(&name) {
+        if self.vars.contains_key(name) {
             return Err(anyhow::anyhow!("Variable already declared!"));
         }
         let mutable = match declaration_type {
             crate::ast::misc::DeclarationType::Mutable => true,
             crate::ast::misc::DeclarationType::Immutable => false,
         };
-        let val = self.expr_eval(value.0)?;
-        self.vars.insert(name, (val, mutable));
+        let val = self.expr_eval(&value.0)?;
+        self.vars.insert(*name, (val, mutable));
         Ok(())
     }
 
     fn if_eval(
         &mut self,
-        condition: Expr,
-        then_branch: Statement,
-        else_branch: Option<Box<Spanned<Statement>>>,
+        condition: &Expr,
+        then_branch: &Vec<Spanned<Statement>>,
+        else_branch: &Option<Vec<Spanned<Statement>>>,
     ) -> anyhow::Result<()> {
-        if let Object::Boolean(b) = self.expr_eval(condition)? {
+        if let Object::Boolean(b) = self.expr_eval(&condition)? {
             if b {
                 self.stmt_eval(then_branch)
             } else if let Some(else_then) = else_branch {
-                self.stmt_eval(else_then.0)
+                self.stmt_eval(else_then)
             } else {
                 Ok(())
             }
@@ -161,26 +160,30 @@ impl Interpreter {
         }
     }
 
-    fn match_eval(&mut self, match_stmt: MatchStmt) -> anyhow::Result<()> {
-        let p_k = self.expr_eval(match_stmt.predicate.0)?; //Possible key
-        let mut found = false;
-        for key_value_pair in match_stmt.then_branches.0 {
-            if let Pattern::Literal(lp) = key_value_pair.0 {
-                if lp.value.0 == p_k {
-                    self.stmt_eval(key_value_pair.1)?;
-                    found = true;
-                    break;
-                }
-            }
-        }
-        Ok(())
-    }
+    //fn match_eval(self, match_stmt: MatchStmt) -> anyhow::Result<()> {
+    //    let p_k = self.expr_eval(match_stmt.predicate.0)?; //Possible key
+    //    let mut found = false;
+    //    for key_value_pair in match_stmt.then_branches.0 {
+    //        if let Pattern::Literal(lp) = key_value_pair.0 {
+    //            if lp.value.0 == p_k {
+    //                self.stmt_eval(key_value_pair.1)?;
+    //                found = true;
+    //                break;
+    //            }
+    //        }
+    //    }
+    //    Ok(())
+    //}
 
-    pub fn while_eval(&mut self, condition: Expr, then_branch: Statement) -> anyhow::Result<()> {
-        if let Object::Boolean(mut b) = self.expr_eval(condition.clone())? {
+    fn while_eval(
+        &mut self,
+        condition: &Expr,
+        then_branch: &Vec<Spanned<Statement>>,
+    ) -> anyhow::Result<()> {
+        if let Object::Boolean(mut b) = self.expr_eval(condition)? {
             while b {
-                self.stmt_eval(then_branch.clone())?;
-                b = self.expr_eval(condition.to_owned())?.into();
+                self.stmt_eval(then_branch)?;
+                b = self.expr_eval(condition)?.into();
             }
             Ok(())
         } else {
@@ -188,41 +191,55 @@ impl Interpreter {
         }
     }
 
-    pub fn stmt_eval(&mut self, stmt: Statement) -> anyhow::Result<()> {
-        match stmt {
-            Statement::Error => return Err(anyhow::anyhow!("Error statment!")),
-            Statement::Block { statments } => self.block_eval(statments)?,
-            Statement::Assign(a) => self.assign_eval(a)?,
-            Statement::Expression { .. } => todo!(),
-            Statement::Declaration {
-                declaration_type,
-                name,
-                manual_type,
-                value,
-            } => self.declar_eval(declaration_type, name.0, manual_type, value)?,
-            Statement::FuncDeclaration { .. } => todo!(),
-            Statement::IfStmt {
-                condition,
-                then_branch,
-                else_branch,
-            } => self.if_eval(condition.0, then_branch.0, else_branch)?,
-            Statement::MatchStmt(match_stmt) => self.match_eval(match_stmt)?,
-            Statement::WhileStmt {
-                condition,
-                then_branch,
-            } => self.while_eval(condition.0, then_branch.0)?,
-            Statement::Defer { .. } => todo!(),
-            Statement::Empty => return Ok(()),
-            Statement::Test(expr) => self.test_eval(expr)?,
+    fn defer_eval(&mut self, defer_stmt: &Spanned<Statement>) -> anyhow::Result<()> {
+        self.defered_stmts.push();
+        Ok(())
+    }
+
+    pub fn stmt_eval(&mut self, stmts: &Vec<Spanned<Statement>>) -> anyhow::Result<()> {
+        for stmt in stmts {
+            match &stmt.0 {
+                Statement::Error => return Err(anyhow::anyhow!("Error statment!")),
+                Statement::Block { statments } => self.block_eval(statments)?,
+                Statement::Assign(a) => self.assign_eval(a)?,
+                Statement::Expression { .. } => (),
+                Statement::Declaration {
+                    declaration_type,
+                    name,
+                    manual_type,
+                    value,
+                } => self.declar_eval(&declaration_type, &name.0, &manual_type, value)?,
+                Statement::FuncDeclaration { .. } => todo!(),
+                Statement::IfStmt {
+                    condition,
+                    then_branch,
+                    else_branch,
+                } => self.if_eval(&condition.0, &then_branch, &else_branch)?,
+                Statement::MatchStmt(_) => todo!(), //self.match_eval(match_stmt)?,
+                Statement::WhileStmt {
+                    condition,
+                    then_branch,
+                } => self.while_eval(&condition.0, &then_branch)?,
+                Statement::Defer { defered_statment } => self.defer_eval(&*defered_statment)?,
+                Statement::Empty => return Ok(()),
+                Statement::Test(expr) => self.test_eval(expr)?,
+            }
         }
         Ok(())
     }
 
-    fn test_eval(&mut self, expr: Expr) -> anyhow::Result<()> {
+    fn test_eval(&mut self, expr: &Expr) -> anyhow::Result<()> {
         let obj = self.expr_eval(expr)?;
         println!("Test Output:{obj}");
         Ok(())
     }
+
+    pub fn interpret(&mut self, stmts: &Vec<Spanned<Statement>>) -> anyhow::Result<()> {
+        self.stmt_eval(stmts)?;
+        // self.stmt_eval(&mut self.defered_stmt)?;
+        Ok(())
+    }
+
     pub fn new() -> Self {
         Self {
             vars: HashMap::new(),
